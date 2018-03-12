adfc4cb9c1ec802348980c8c2e79fddf4915b2cd
Gigabytez <36308998+Gigabytez@users.noreply.github.com>
2018-03-11 22:37:26 -0700

    Added a Transaction WPF screen for the Transaction table to be displayed on.

---
+++ Database/Transactions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 
+ * 
+ * 
+ */
+namespace Database
+{
+    class Transactions
+    {   [Key]
+        // This will be the key for the TR table.
+        public int TrNumber { get; set; }
+
+        public int RoomNo { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
