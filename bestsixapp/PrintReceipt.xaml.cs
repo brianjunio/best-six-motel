bb2fe61660f32b5c847274d7c09a6b047a8621fe
Kevin Smith <KevinESmith@outlook.com>
2018-04-10 13:38:26 -0700

    PrintReceipt

---
+++ bestsixapp/PrintReceipt.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bestsixapp
+{
+    /// <summary>
+    /// Interaction logic for PrintReceipt.xaml
+    /// </summary>
+    public partial class PrintReceipt : Window
+    {
+        public PrintReceipt()
+        {
+            InitializeComponent();
+        }
+    }
+}
