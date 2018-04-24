f046fb90a3809c1d141687beda59eb4d2e27b63f
Gigabytez <36308998+Gigabytez@users.noreply.github.com>
2018-04-24 03:34:03 -0700

    Re-added Transaction/TransactionView changes and tested to make sure it worked properly.

--- bestsixapp/TransactionsView.xaml.cs
+++ bestsixapp/TransactionsView.xaml.cs
@@ -16,18 +16,21 @@ namespace bestsixapp
     /// </summary>
     public partial class TransactionsView : Page
     {
+        DateTime StartDate, EndDate;
+        Room roomData = new Room();
+        Customer custData = new Customer();
+        Transactions tr = new Transactions();
+
         public TransactionsView()
         {
             InitializeComponent();
             trDataGrid.ItemsSource = LoadTransactions();
-            
         }
 
         /*
          *  Transforms transaction table into a list. 
          *  List of Transactions become the data source of the data grid.
          */
-
         private List<Transactions> LoadTransactions()
         {
             List<Transactions> transactionList = new List<Transactions>();
@@ -40,13 +43,92 @@ namespace bestsixapp
             return transactionList;
         }
 
-        private void TRClose_Click(object sender, RoutedEventArgs e)
+        /*
+         * Transforms transaction table into a list. 
+         * Date Range choices of Transactions becomes the search key for source of the data grid.
+         */
+        private List<Transactions> LoadDateRangeTransactions()
         {
-            //this.Close();
-            //Main.NavigationService.Navigate(roomWindow);
-            this.NavigationService.Navigate(this.Parent);
+            using (DatabaseContext dbContext = new DatabaseContext())
+            {
+                // Look for values that match the searched date range.
+                var trDateRangeList = dbContext.Transactions.Where(x => (x.Room.Checkin >= StartDate && x.Room.Checkin <= EndDate)
+                                                                              || (x.Room.Checkout >= StartDate && x.Room.Checkout <= EndDate));
+                var result = trDateRangeList.ToList();
+                if (!result.Any())
+                {
+                    System.Windows.Forms.MessageBox.Show("There are no records for that date range.");
+                    return null;
+                }
+                else
+                {
+                    return result;
+                }
+            }
         }
 
+        /*
+         * Transforms transaction table into a list. 
+         * License of customer in Transactions become the search key for source of the data grid. 
+         */
+        private List<Transactions> LoadLicenseTransactions()
+        {
+            //List<Transactions> trLicenseList = new List<Transactions>();
 
+            using (DatabaseContext dbContext = new DatabaseContext())
+            {
+                // Look for values that match the searched driver's license ID.
+                var trLicenseList = dbContext.Transactions.Where(x => x.ID == CustomerLicense.Text);
+                var result = trLicenseList.ToList();
+                if (!result.Any())
+                {
+                    System.Windows.Forms.MessageBox.Show("There are no records for that License.");
+                    return null;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+        }
+
+        // Grab datepicker StartDate's value and make it a DateTime Obj.
+        private void StartDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            StartDate = (DateTime)Start.SelectedDate;
+            startingDate.Text = StartDate.ToLongDateString().ToString();
+            startingDate.IsEnabled = false;
+        }
+        // Grab datepicker EndDate's value and make it a DateTime Obj.
+        private void EndDateChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            EndDate = (DateTime)End.SelectedDate;
+            endingDate.Text = EndDate.ToLongDateString().ToString();
+            endingDate.IsEnabled = false;
+        }
+
+        /*
+         * Load Date Range button functionality.
+         */
+
+        public void Load_button_click(object sender, RoutedEventArgs e)
+        {
+            trDataGrid.ItemsSource = LoadDateRangeTransactions();
+        }
+
+        /*
+         * Search Driver's License button functionality.
+         */
+
+        public void Search_button_click(object sender, RoutedEventArgs e)
+        {
+            trDataGrid.ItemsSource = LoadLicenseTransactions();
+        }
+
+        // Closes window back to the 'Parent' page which is home.
+        private void TRClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(this.Parent);
+        }
     }
 }
